f7a3d1d69bc3dfd520120d94e87b7107322f160b
Sara Elajab <eng.saraelajab@gmail.com>
2021-10-24 16:32:22 +0200

    Original source code copied from desktop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,5 +16,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+		if (env('APP_ENV') != 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
