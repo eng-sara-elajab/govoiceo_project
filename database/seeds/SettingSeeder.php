e36684698c4ded4720446636de40c8599cc83d61
Sara Elajab <eng.saraelajab@gmail.com>
2022-08-01 17:45:36 +0200

    Original source code copied from desktop

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -17,7 +17,9 @@ class SettingSeeder extends Seeder
             'email' => 'websitename@original.com',
             'phone' => '00966502371248',
             'address' => '198 West 21th Street, Suite 721 New York NY 10016',
-            'description' => 'موقع لشرح احترافي للكورسات في مختلف المجالات مع مدربين ذوو مقدرات عالية',
+            'description' => 'The School of Continuing Studies online courses are instructor-led and delivered primarily through an online course site using one or more collaborative, communication and/or social technologies. If the course you are  interested in is offered in multiple formats (in-class, online or hybrid) be assured that these formats use the same learning materials and will allow you to achieve the same learning outcomes.
+
+Three days before the course start date, you will receive an email with detailed  instructions on how to access your online course site. If you register less than five days before the course start, you will receive the access information three days after registration. The email may be filtered to your junk mail, so please check there as well. If you do not  receive the access information, please submit a help request, speak to us through our LiveHelp, or give us a call.',
             'logo' => 'logo.jpg',
             'twitter_link' => 'https://twitter.com/login',
             'facebook_link' => 'https://web.facebook.com/?_rdc=1&_rdr',
